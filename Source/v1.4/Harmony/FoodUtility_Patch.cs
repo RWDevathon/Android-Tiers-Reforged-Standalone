3f5f9a37a053c5b39ee1cb07487536c37ff23437
Killathon <huelva21@gmail.com>
2022-11-27 11:07:28 -0800

    1.3/1.4 fix for all food poisoning cases using vanilla calculations

---
+++ Source/v1.4/Harmony/FoodUtility_Patch.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+
+namespace ATReforged
+{
+    // Mechanical units can not be food poisoned, so set their chance to receive food poisoning to zero.
+    internal class FoodUtility_Patch
+    {
+        [HarmonyPatch(typeof(FoodUtility), "GetFoodPoisonChanceFactor")]
+        public class GetFoodPoisonChanceFactor_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(Pawn ingester, ref float __result)
+            {
+                if (Utils.IsConsideredMechanical(ingester))
+                    __result = 0f;
+            }
+        }
+    }
+}
