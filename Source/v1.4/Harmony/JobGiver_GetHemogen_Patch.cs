f814bbbee16c4afeb37f080a43cfc3259ff478a9
Killathon <huelva21@gmail.com>
2022-12-03 19:36:01 -0800

    1.4 Tiny fix for GetHemogen patch

--- Source/v1.4/Harmony/JobGiver_GetHemogen_Patch.cs
+++ Source/v1.4/Harmony/JobGiver_GetHemogen_Patch.cs
@@ -13,7 +13,7 @@ namespace ATReforged
         public class CanFeedOnPrisoner_Patch
         {
             [HarmonyPostfix]
-            public static void Listener(ref bool __result, Pawn prisoner)
+            public static void Listener(ref AcceptanceReport __result, Pawn prisoner)
             {
                 __result = __result && !Utils.IsConsideredMechanical(prisoner);
             }
