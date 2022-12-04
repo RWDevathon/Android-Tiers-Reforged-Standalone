f9010f33a10b40cf949d5b0b1a6101452bfaff9b
Killathon <huelva21@gmail.com>
2022-12-03 18:37:18 -0800

    1.4 Prevented hemogen gain from mechanical units

---
+++ Source/v1.4/Harmony/CompAbilityEffect_BloodfeederBite_Patch.cs
@@ -0,0 +1,22 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Linq;
+using RimWorld.Planet;
+using RimWorld;
+
+namespace ATReforged
+{
+    internal class CompAbilityEffect_BloodfeederBite_Patch
+    {
+        // Mechanical units are invalid targets for blood feeding.
+        [HarmonyPatch(typeof(CompAbilityEffect_BloodfeederBite), "Valid")]
+        public class Valid_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(ref bool __result, LocalTargetInfo target)
+            {
+                __result = __result && !Utils.IsConsideredMechanical(target.Pawn);
+            }
+        }
+    }
+}
---
+++ Source/v1.4/Harmony/JobGiver_GetHemogen_Patch.cs
@@ -0,0 +1,22 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Linq;
+using RimWorld.Planet;
+using RimWorld;
+
+namespace ATReforged
+{
+    internal class JobGiver_GetHemogen_Patch
+    {
+        // Mechanical units are invalid targets for blood feeding.
+        [HarmonyPatch(typeof(JobGiver_GetHemogen), "CanFeedOnPrisoner")]
+        public class CanFeedOnPrisoner_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(ref bool __result, Pawn prisoner)
+            {
+                __result = __result && !Utils.IsConsideredMechanical(prisoner);
+            }
+        }
+    }
+}
