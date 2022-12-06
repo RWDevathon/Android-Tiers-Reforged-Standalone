15d0637f1b1c7fefbca341abe2905bce5a0b723a
Killathon <huelva21@gmail.com>
2022-12-05 18:03:58 -0800

    1.4 Androids now spawn with sufficient food

--- Source/v1.4/Harmony/PawnGroupMakerUtility_Patch.cs
+++ Source/v1.4/Harmony/PawnGroupMakerUtility_Patch.cs
@@ -29,16 +29,6 @@ namespace ATReforged
                         {
                             nbHumanoids++;
                         }
-
-                        Hediff stasisHediff = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.ATR_StasisPill);
-                        if (Utils.IsConsideredMechanical(pawn) && stasisHediff == null)
-                        {
-                            pawn.health.AddHediff(HediffMaker.MakeHediff(HediffDefOf.ATR_StasisPill, pawn));
-                        }
-                        else if (stasisHediff != null)
-                        {
-                            stasisHediff.Severity = 1f;
-                        }
                     }
 
                     // Skip factions not allowed to use surrogates or groups that are too small
@@ -239,7 +229,7 @@ namespace ATReforged
                     // All members of the group that were unaltered are returned to the final grouping.
                     __result = unalteredGroup.Concat(ret);
 
-                    // Generated mechanical pawns in groups will always receive the Stasis Hediff to reduce their power consumption significantly so they don't run into malnutrition issues as frequently.
+                    // Generated mechanical pawns in proper groups will always receive the Stasis Hediff to reduce their power consumption significantly.
                     foreach (Pawn member in __result)
                     {
                         Hediff stasisHediff = member.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.ATR_StasisPill);
