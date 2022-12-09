886ed3fe0609e151cc1bdb274e76f5878603c32b
Killathon <huelva21@gmail.com>
2022-12-09 11:01:58 -0800

    1.4 Fix for surrogate interactions, android meds, refugees

--- Source/v1.4/Harmony/DownedRefugeeQuestUtility_Patch.cs
+++ Source/v1.4/Harmony/DownedRefugeeQuestUtility_Patch.cs
@@ -13,9 +13,18 @@ namespace ATReforged
             [HarmonyPostfix]
             public static void Listener(int tile, ref Pawn __result, PawnKindDef pawnKind = null, float chanceForFaction = 0.6f)
             {
-                if (__result != null && __result.Faction != null && Utils.ReservedAndroidFactions.Contains(__result.Faction.def.defName) && !Utils.IsConsideredMechanical(__result))
+                // No organics in android factions.
+                if (__result != null && __result.Faction != null  && Utils.ReservedAndroidFactions.Contains(__result.Faction.def.defName) && !Utils.IsConsideredMechanical(__result))
                 {
                     Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(__result.Faction.def.basicMemberKind, __result.Faction, PawnGenerationContext.NonPlayer, tile, forceGenerateNewPawn: true, canGeneratePawnRelations: false, allowFood: true));
+                    HealthUtility.DamageUntilDowned(pawn, allowBleedingWounds: false);
+                    HealthUtility.DamageLegsUntilIncapableOfMoving(pawn, allowBleedingWounds: false);
+                    __result = pawn;
+                }
+                // Factionless refugees for android faction players will be androids.
+                else if (__result.Faction == null && Faction.OfPlayer.def.defName == "PlayerColonyAndroid")
+                {
+                    Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(Faction.OfPlayer.def.basicMemberKind, null, PawnGenerationContext.NonPlayer, tile, forceGenerateNewPawn: true, canGeneratePawnRelations: false, allowFood: true));
                     HealthUtility.DamageUntilDowned(pawn, allowBleedingWounds: false);
                     HealthUtility.DamageLegsUntilIncapableOfMoving(pawn, allowBleedingWounds: false);
                     __result = pawn;
