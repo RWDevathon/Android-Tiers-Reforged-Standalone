a6a78028bdd710fb4299253c82a98b018204ba48
Killathon <huelva21@gmail.com>
2023-05-12 18:34:21 -0700

    Small SkyMind fix

--- Source/v1.4/Harmony/WorldPawns_Patch.cs
+++ Source/v1.4/Harmony/WorldPawns_Patch.cs
@@ -1,17 +1,18 @@
 ﻿using Verse;
 using HarmonyLib;
 using RimWorld.Planet;
+using RimWorld;
 
 namespace ATReforged
 {
-    public class WorldPawns_Patch
+    // Non-player pawns being passed to the world can never be connected to the SkyMind network (they may have just been kidnapped, sold, or lost).
+    [HarmonyPatch(typeof(WorldPawns), "PassToWorld")]
+    public class PassToWorld_Patch
     {
-        // Pawns being passed to the world can never be connected to the SkyMind network (they're no longer player pawns).
-        [HarmonyPatch(typeof(WorldPawns), "PassToWorld")]
-        public class PassToWorld_Patch
+        [HarmonyPostfix]
+        public static void Listener(Pawn pawn, PawnDiscardDecideMode discardMode)
         {
-            [HarmonyPostfix]
-            public static void Listener(Pawn pawn, PawnDiscardDecideMode discardMode)
+            if (pawn.Faction != Faction.OfPlayer)
             {
                 // This is an always safe operation, only pawns that are stored will actually be removed.
                 Utils.gameComp.DisconnectFromSkyMind(pawn);
